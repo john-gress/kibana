0086da5ae11e6d3e985caff2a59f7890c8cca5e3
Craig Cogdill <craig.cogdill@logrhythm.com>
2020-03-04 16:53:08 -0700

    Force typescript to accept backslashes in the names of downloadable files

--- src/netmon/field_formats/boolean_formats.ts
+++ src/netmon/field_formats/boolean_formats.ts
@@ -12,10 +12,14 @@ export const formatAttachDownload = (value: boolean, field: any, hit: any) => {
   const session = hit && hit._source && hit._source.Session ? hit._source.Session : '';
 
   const fileName = hit && hit._source && hit._source.FileGenericName ? hit._source.FileGenericName : '';
+  // 'fileName' is not a string type. Attempts to pass it with backslashes in the filename will
+  // cause typescript to eat the backslashes on the next function call. Instead, toString it and
+  // force the backlash to be escaped, so that it is correctly interpreted later
+  const escapedFileName = fileName.toString().replace('\\', '\\\\');
 
   const captured = hit && hit._source && hit._source.Captured ? true : false;
 
-  return `<attach-download session="'${session}'" fileName="'${fileName}'" captured="${captured}">true</attach-download>`;
+  return `<attach-download session="'${session}'" fileName="'${escapedFileName}'" captured="${captured}">true</attach-download>`;
 };
 
 export const formatCaptureDownload = (value: boolean, field: any, hit: any) => {
